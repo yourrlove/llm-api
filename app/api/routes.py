7a6a8de5b8a3246d3ad1d89fb7cbd7b9754f858f
yourrlove <nanhvt2708@gmail.com>
2024-08-11 14:22:34 +0700

    first commit

--- app/api/routes.py
+++ app/api/routes.py
@@ -11,6 +11,10 @@ from app.services.ingest_data import ingest
 from app import PROJECT_PATH, DATA_DIR, DEFAULT_DATA_PATH
 from app.models.model import llm, embed_model
 
+# get result by query
+# data path is data directory at local
+# step1: construct or initialize query engine (query tools, retrieve tool)
+# step2: retrieve result by using query engine search in database by data directory
 @api_blueprint.route('/ai/v1/llm/assets/<query>', methods=['GET'])
 def get_response_by_query(query, data_path=DEFAULT_DATA_PATH):
     query_engine = construct_router_query_engine()
@@ -21,6 +25,7 @@ def get_response_by_query(query, data_path=DEFAULT_DATA_PATH):
         print(output)
         return jsonify(output)
 
+# similar to get_response_by_query but have a different query engine to generate content: qyery_engine_generate 
 @api_blueprint.route('/ai/v1/llm/articles/<query>', methods=['GET'])
 def generate_content_by_query(query):
     response = generate_content(query_engine_generate, query)
@@ -30,7 +35,11 @@ def generate_content_by_query(query):
         print(response)
         return jsonify(response)
 
-
+# upload data: 
+# upload new data in the folder at DATA_DIR location
+# then ingest all data in the folder at DATA_DIR location
+# ingest() => build ingestion pipeline => classified, indexed....
+# return number of ingested node in each document type
 @api_blueprint.route('/ai/v1/llm/data')
 def upload():
     # Extract parameters from the request
---
+++ test.py
@@ -0,0 +1,2 @@
+import os
+os.environ['HF_HOME'] = 'D:/cache/'
